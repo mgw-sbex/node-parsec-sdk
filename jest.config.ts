ad64c3961eefa4d3bfd2aceb4c9f4671a5aabca6
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-26 22:36:47 +0200

    Configure ESLint, Prettier and Jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
+};
